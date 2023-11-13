c6b71b3ec861b511da5c98c4fea85f95af48d79f
bombies <xjamesdzn@gmail.com>
2023-11-13 13:14:57 -0500

    Setup hot-reloading

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import configuration from "./utils/config/configuration";
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(configuration().PORT);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 
 bootstrap();
