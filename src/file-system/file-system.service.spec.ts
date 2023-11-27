376b7aea319117e9cf0d90e234bc4e573885a10c
bombies <xjamesdzn@gmail.com>
2023-11-27 16:39:04 -0500

    Add fileExists test

--- src/file-system/file-system.service.spec.ts
+++ src/file-system/file-system.service.spec.ts
@@ -2,17 +2,27 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { FileSystemService } from './file-system.service';
 
 describe('FileSystemService', () => {
-  let service: FileSystemService;
+    let service: FileSystemService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [FileSystemService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [FileSystemService],
+        }).compile();
 
-    service = module.get<FileSystemService>(FileSystemService);
-  });
+        service = module.get<FileSystemService>(FileSystemService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    describe('fileExists', () => {
+        it('should return false when a file or directory does not exist', async () => {
+            expect(service.fileExists('/doesnt-exist')).toBe(false);
+        });
+
+        it('should return true when a file or directory exists', async () => {
+            expect(service.fileExists('README.md')).toBe(true);
+        });
+    });
 });
