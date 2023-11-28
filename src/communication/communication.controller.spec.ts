5837496f33b0f7c56308d4804669c979395c15ec
bombies <xjamesdzn@gmail.com>
2023-11-28 14:40:14 -0500

    Fix tests

--- src/communication/communication.controller.spec.ts
+++ src/communication/communication.controller.spec.ts
@@ -4,6 +4,7 @@ import { JwtService } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
 import { PrismaService } from "../utils/database/prisma.service";
 import { CommunicationService } from "./communication.service";
+import { MemberService } from '../member/member.service';
 
 describe('CommunicationController', () => {
   let controller: CommunicationController;
@@ -11,7 +12,7 @@ describe('CommunicationController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommunicationController],
-      providers: [JwtService, ConfigService, CommunicationService, PrismaService]
+      providers: [JwtService, ConfigService, CommunicationService, PrismaService, MemberService]
     }).compile();
 
     controller = module.get<CommunicationController>(CommunicationController);
--- src/communication/communication.service.spec.ts
+++ src/communication/communication.service.spec.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommunicationService } from './communication.service';
 import { PrismaService } from "../utils/database/prisma.service";
+import { MemberService } from '../member/member.service';
 
 describe('CommunicationService', () => {
   let service: CommunicationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CommunicationService, PrismaService],
+      providers: [CommunicationService, PrismaService, MemberService],
     }).compile();
 
     service = module.get<CommunicationService>(CommunicationService);
